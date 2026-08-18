954804622c7bde491c43aad58093c4686a301311
Bastian Moraga <bastian.moraga01@gmail.com>
2026-08-18 19:37:48 -0400

    first commit

---
+++ src/presentation/server.ts
@@ -0,0 +1,28 @@
+import { CheckService } from "../domain/use-cases/checks/check-service";
+import { CronService } from "./cron/cron-service";
+
+
+export class Server {
+
+    public static start() {
+
+        console.log('Server started...');
+
+        CronService.createJob(
+            '*/10 * * * * *',
+            () => {
+
+                // new CheckService().execute('http://localhost:3000/posts');
+                const url = 'https://google.com';
+                new CheckService(
+                    () => console.log(`${ url } is ok!`),
+                    (error) => console.log(error)
+                ).execute(url);
+
+            }
+        );
+
+
+    }
+
+}
