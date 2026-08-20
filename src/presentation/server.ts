a62decddb0fbc0e2ebfb966aa4c710bd1f94c617
Bastian Moraga <bastian.moraga01@gmail.com>
2026-08-20 02:04:27 -0400

    feat(noc): implement filesystem datasource with repository pattern

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,6 +1,11 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { FileSystemDataSource } from "../infrastructure/datasources/file-system.datasource";
+import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
 
+const fileSystemLogRepository = new LogRepositoryImpl(
+    new FileSystemDataSource()
+);
 
 export class Server {
 
@@ -13,8 +18,10 @@ export class Server {
             () => {
 
                 // new CheckService().execute('http://localhost:3000/posts');
+                // const url = 'http://localhost:3000/posts';
                 const url = 'https://google.com';
                 new CheckService(
+                    fileSystemLogRepository,
                     () => console.log(`${ url } is ok!`),
                     (error) => console.log(error)
                 ).execute(url);
