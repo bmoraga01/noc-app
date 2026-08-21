68a964c63bb3532147b16032adb27b8ee6fcd9ad
Bastian Moraga <bastian.moraga01@gmail.com>
2026-08-20 21:20:01 -0400

    feat(noc): add email log notifications with Nodemailer

---
+++ src/presentation/email/email.service.ts
@@ -0,0 +1,83 @@
+import nodemailer from "nodemailer";
+import { envs } from "../../config/plugins/envs.plugin";
+import { LogEntity, LogSeverityLevel } from "../../domain/entities/log.entity";
+
+interface SendMailOptions {
+    to: string | string[];
+    subject: string;
+    htmlBody: string;
+    attachments?: Attachments[];
+}
+
+interface Attachments {
+    filename: string;
+    path: string;
+}
+
+export class EmailService {
+
+    private transporter = nodemailer.createTransport({
+        service: envs.MAILER_SERVICE,
+        auth: {
+            user: envs.MAILER_EMAIL,
+            pass: envs.MAILER_SECRET_KEY,
+        }
+    })
+
+    constructor() {}
+
+    async sendEmail( options: SendMailOptions ): Promise<boolean> {
+
+        const { to, subject, htmlBody, attachments = [] } = options;
+
+        try {
+
+            const sendInformation = await this.transporter.sendMail({
+                to,
+                subject,
+                html: htmlBody,
+                attachments,
+            })
+
+            const log = new LogEntity({
+                level: LogSeverityLevel.low,
+                message: 'Email sent',
+                origin: 'email.service.ts',
+            })
+
+            // console.log(sendInformation);
+
+            return true;
+        } catch (error) {
+
+            const log = new LogEntity({
+                level: LogSeverityLevel.high,
+                message: 'Email not sent',
+                origin: 'email.service.ts',
+            })
+
+            return false;
+        }
+
+    }
+
+    async sendEmailWithFileSystemLogs( to: string | string[] ) {
+        const subject = 'Logs del servidor';
+        const htmlBody = `
+            <h3>Logs de sistema - NOC</h3>
+            <p>Excepteur nostrud anim sint ut est eu id fugiat reprehenderit ex et ea duis fugiat. Nisi aliquip adipisicing adipisicing laboris nulla deserunt reprehenderit. Mollit dolor dolor tempor pariatur non elit exercitation. Sunt minim incididunt id duis enim ullamco cillum laborum esse. Ipsum quis magna sit veniam ad mollit cillum ipsum ipsum cillum duis. Eiusmod enim labore enim Lorem elit laboris sit cillum ut Lorem qui consectetur consectetur.</p>
+            <p>Ver logs adjuntos</p>
+        `;
+
+        const attachments: Attachments[] = [
+            { filename: 'logs-all.log', path: './logs/logs-all.log' },
+            { filename: 'logs-medium.log', path: './logs/logs-medium.log' },
+            { filename: 'logs-high.log', path: './logs/logs-high.log' },
+        ];
+
+        return this.sendEmail({
+            to, subject, htmlBody, attachments
+        })
+    }
+
+}
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,11 +1,14 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
 import { FileSystemDataSource } from "../infrastructure/datasources/file-system.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
+import { EmailService } from "./email/email.service";
 
 const fileSystemLogRepository = new LogRepositoryImpl(
     new FileSystemDataSource()
 );
+const emailService = new EmailService();
 
 export class Server {
 
@@ -13,21 +16,31 @@ export class Server {
 
         console.log('Server started...');
 
-        CronService.createJob(
-            '*/10 * * * * *',
-            () => {
+        // todo: Mandar Email
+        // new SendEmailLogs(
+        //     emailService,
+        //     fileSystemLogRepository
+        // ).execute(
+        //         // ['bastiedumoraga@gmail.com', '1230984756lauty@gmail.com']
+        //         'bastiedumoraga@gmail.com'
+        // )
 
-                // new CheckService().execute('http://localhost:3000/posts');
-                // const url = 'http://localhost:3000/posts';
-                const url = 'https://google.com';
-                new CheckService(
-                    fileSystemLogRepository,
-                    () => console.log(`${ url } is ok!`),
-                    (error) => console.log(error)
-                ).execute(url);
 
-            }
-        );
+        // CronService.createJob(
+        //     '*/3 * * * * *',
+        //     () => {
+
+        //         // const url = 'http://localhost:3000/posts';
+        //         const url = 'https://pokeapi.co/api/v2/pokemon/1';
+        //         // const url = 'https://google.com';
+        //         new CheckService(
+        //             fileSystemLogRepository,
+        //             () => console.log(`${ url } is ok!`),
+        //             (error) => console.log(error)
+        //         ).execute(url);
+
+        //     }
+        // );
 
 
     }
